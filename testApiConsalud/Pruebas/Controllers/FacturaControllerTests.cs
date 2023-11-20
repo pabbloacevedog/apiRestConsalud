9707e4c59409f37704e2e744e8a914733e8cdd55
Pablo Acevedo González <pablo.acevedo.g@gmail.com>
2023-11-20 02:46:08 +0000

    se intenta publicar en azure

---
+++ testApiConsalud/Pruebas/Controllers/FacturaControllerTests.cs
@@ -0,0 +1,118 @@
+﻿using Xunit;
+using Moq; // Asegúrate de tener instalado el paquete NuGet Moq para poder usarlo
+using Microsoft.AspNetCore.Mvc;
+using apiRestConsalud.Controllers;
+using apiConsalud.Services;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace testApiConsalud.Pruebas.Controllers
+{
+    public class FacturaControllerTests
+    {
+        [Fact]
+        public void GetFacturas_DebeRetornarFacturasFormateadas()
+        {
+            // Configurar el servicio con un mock
+            var mockFacturaService = new Mock<FacturaService>();
+            mockFacturaService.Setup(service => service.GetFacturas()).Returns(new JArray()); 
+
+            // Crear una instancia del controlador con el servicio mock
+            var controller = new FacturaController(mockFacturaService.Object);
+
+            // Act
+            var result = controller.GetFacturas();
+
+            // Verificar el resultado
+            Assert.IsType<OkObjectResult>(result.Result); // Verificar si el resultado es un Ok
+            var okResult = (OkObjectResult)result.Result;
+            Assert.IsType<JArray>(okResult.Value); // Verificar si el valor es un JArray
+        }
+
+        [Fact]
+        public void GetFacturas_ReturnsOkResult()
+        {
+            // Arrange
+            var facturaServiceMock = new Mock<FacturaService>();
+            facturaServiceMock.Setup(service => service.GetFacturas()).Returns(new JArray()); 
+
+            var controller = new FacturaController(facturaServiceMock.Object);
+
+            // Act
+            var result = controller.GetFacturas();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(okResult.Value); // Verifica que el valor no sea nulo
+        }
+
+        [Fact]
+        public void GetFacturasPorComprador_ReturnsOkResult()
+        {
+            // Arrange
+            var facturaServiceMock = new Mock<FacturaService>();
+            facturaServiceMock.Setup(service => service.GetFacturasPorComprador(It.IsAny<double>())).Returns(new JArray());
+
+            var controller = new FacturaController(facturaServiceMock.Object);
+
+            // Act
+            var result = controller.GetFacturasPorComprador("12345678"); // Proporciona un RUT válido
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(okResult.Value);
+        }
+
+        [Fact]
+        public void GetCompradorConMasCompras_ReturnsOkResult()
+        {
+            // Arrange
+            var facturaServiceMock = new Mock<FacturaService>();
+            facturaServiceMock.Setup(service => service.GetCompradorConMasCompras()).Returns(new JObject());
+
+            var controller = new FacturaController(facturaServiceMock.Object);
+
+            // Act
+            var result = controller.GetCompradorConMasCompras();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(okResult.Value);
+        }
+
+        [Fact]
+        public void GetCompradoresConMontoTotalDeCompras_ReturnsOkResult()
+        {
+            // Arrange
+            var facturaServiceMock = new Mock<FacturaService>();
+            facturaServiceMock.Setup(service => service.GetCompradoresConMontoTotalDeCompras()).Returns(new JArray());
+
+            var controller = new FacturaController(facturaServiceMock.Object);
+
+            // Act
+            var result = controller.GetCompradoresConMontoTotalDeCompras();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(okResult.Value);
+        }
+
+        [Fact]
+        public void GetFacturasAgrupadasPorComuna_ReturnsOkResult()
+        {
+            // Arrange
+            var facturaServiceMock = new Mock<FacturaService>();
+            facturaServiceMock.Setup(service => service.GetFacturasAgrupadasPorComuna(It.IsAny<double>())).Returns(new JArray());
+
+            var controller = new FacturaController(facturaServiceMock.Object);
+
+            // Act
+            var result = controller.GetFacturasAgrupadasPorComuna(1); // Proporciona un valor de comuna válido
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.NotNull(okResult.Value);
+        }
+    }
+}
---
+++ testApiConsalud/Pruebas/Services/FacturaServiceTests.cs
@@ -0,0 +1,87 @@
+﻿using Xunit;
+using Moq;
+using apiConsalud.Services;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace testApiConsalud.Pruebas.Services
+{
+    public class FacturaServiceTests
+    {
+        [Fact]
+        public void GetFacturas_DebeRetornarFacturas()
+        {
+            // Configurar el servicio con un mock
+            var mockFilePath = "BD/JsonEjemplo.json";
+            var mockJsonString = File.ReadAllText(mockFilePath);
+            var mockFacturas = JArray.Parse(mockJsonString);
+
+            var mockFacturaService = new Mock<FacturaService>();
+            mockFacturaService.Setup(service => service.GetFacturas()).Returns(mockFacturas);
+
+            // Actuar - Llamar al método de prueba
+            var result = mockFacturaService.Object.GetFacturas();
+
+            // Afirmar - Verificar el resultado
+            Assert.NotNull(result);
+            Assert.IsType<JArray>(result);
+        }
+
+        [Fact]
+        public void GetFacturas_ReturnsCorrectFacturas()
+        {
+            // Arrange
+            var facturaService = new FacturaService();
+
+            // Act
+            var result = facturaService.GetFacturas();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void GetCompradorConMasCompras_ReturnsCorrectComprador()
+        {
+            // Arrange
+            var facturaService = new FacturaService();
+
+            // Act
+            var result = facturaService.GetCompradorConMasCompras();
+
+            // Assert
+            Assert.NotNull(result);
+            
+        }
+
+        [Fact]
+        public void GetCompradoresConMontoTotalDeCompras_ReturnsCorrectCompradores()
+        {
+            // Arrange
+            var facturaService = new FacturaService();
+
+            // Act
+            var result = facturaService.GetCompradoresConMontoTotalDeCompras();
+
+            // Assert
+            Assert.NotNull(result);
+            
+        }
+
+        [Fact]
+        public void GetFacturasAgrupadasPorComuna_ReturnsCorrectFacturas()
+        {
+            // Arrange
+            var facturaService = new FacturaService();
+            double comuna = 123;  // Proporciona un valor de comuna válido
+
+            // Act
+            var result = facturaService.GetFacturasAgrupadasPorComuna(comuna);
+
+            // Assert
+            Assert.NotNull(result);
+            
+        }
+    }
+}
